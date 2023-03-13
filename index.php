3557bfb4588e7753da13f1343cdb0a7344c4b521
Nicola-Campagna <campagnanicola379@gmail.com>
2023-03-13 18:03:26 +0100

    milestone 1=password generatore

--- index.php
+++ index.php
@@ -10,6 +10,46 @@
 </head>
 
 <body>
+
+    <form action="" method="get">
+
+        <label for="password-length">Lunghezza Password:</label>
+        <input type="number" id="password-length" name="password-length" min="0" max="12" required>
+
+        <button type="submit">INVIA</button>
+
+    </form>
+
+
+    <div>
+        <?php 
+        $passwordlen=$_GET["password-length"]??"5
+        ";
+
+        // funzione genera password random 
+        function randomPassword($passwordlen){
+            
+        $password_caracters="ABCDEFGKILMNOPQRSTUVZabcdefghilmnopqrstuvz1234567890!£$%&/()=?";
+        $password=array();
+        $password_caracters_length=strlen($password_caracters)-1;
+
+        for($i=0; $i<$passwordlen;$i++){
+        $n=rand(0,$password_caracters_length);
+        $password[]=$password_caracters[$n];
+        };
+
+        return implode($password);
+
+        }
+        ?>
+
+        <h1>PASSWORD GENERATA: <?= randomPassword($passwordlen)?></h1>
+
+    </div>
+
+
+
+
 </body>
 
 </html>
